c9e7815402071c023cf26b426df2b3de86443648
Rani <rani@innocirc.com>
2020-04-27 15:44:36 +0530

    bmi calcultor

---
+++ BMICalculator/Model/CalculatorModel.swift
@@ -0,0 +1,9 @@
+//
+//  CalculatorModel.swift
+//  BMICalculator
+//
+//  Created by Rani Singh on 27/04/20.
+//  Copyright © 2020 InnoCric. All rights reserved.
+//
+
+import Foundation
---
+++ BMICalculator/ViewController/ResultViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResultViewController.swift
+//  BMICalculator
+//
+//  Created by Rani Singh on 27/04/20.
+//  Copyright © 2020 InnoCric. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
